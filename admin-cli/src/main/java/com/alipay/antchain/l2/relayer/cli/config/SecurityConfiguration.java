42f9704c49cfa8168be34bc5305d2becb388d48d
zouxyan <zouxueyan.zxy@antgroup.com>
2026-01-09 18:11:44 +0800

    feat: add basic auth between admin client and server

---
+++ admin-cli/src/main/java/com/alipay/antchain/l2/relayer/cli/config/SecurityConfiguration.java
@@ -0,0 +1,22 @@
+package com.alipay.antchain.l2.relayer.cli.config;
+
+import io.grpc.CallCredentials;
+import net.devh.boot.grpc.client.security.CallCredentialsHelper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration(proxyBeanMethods = false)
+public class SecurityConfiguration {
+
+    @Value("${auth.username}")
+    private String username;
+
+    @Value("${auth.password}")
+    private String password;
+
+    @Bean
+    CallCredentials grpcCredentials() {
+        return CallCredentialsHelper.basicAuth(this.username, this.password);
+    }
+}
